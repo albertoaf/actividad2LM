de84dc88686f5eae43f70dc3b5dac45211ccb6af
albertoaf <albertoaf.formacion@gmail.com>
2017-04-04 00:25:54 +0200

    Formulario PHP

---
+++ server/contacto.php
@@ -0,0 +1,6 @@
+<?php
+print "Formulario procesado con los siguientes datos\n";
+print "Nombre: $_REQUEST[nombre]\n";
+print "E-mail: $_REQUEST[email]\n";
+print "Mensaje: $_REQUEST[mensaje]\n";
+?>
